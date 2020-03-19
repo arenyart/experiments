c708b439f2fe8099b98cdb43a0d7c6211b6b0f03
arenyart <arenyart@gmail.com>
2020-03-19 09:55:19 -0400

    slight tweak

--- pypaint.py
+++ pypaint.py
@@ -1,20 +1,21 @@
 import os
 import random
-import sys
+
 import pygame as pg
 
 
 class App:
 
-    def __init__(self,imgWidth,imgHeight):
+    def __init__(self):
         os.environ['SDL_VIDEO_CENTERED'] = '1'
         pg.init()
-        self.screen = pg.display.set_mode((imgWidth, imgHeight))
-        self.screen.fill(pg.Color(0,0,0))
+        self.w, self.h = 640, 480
+        self.screen = pg.display.set_mode((self.w, self.h))
+        self.screen.fill(pg.Color('white'))
         self.clock = pg.time.Clock()
-        self.drawcolor = (255,255,255)
+        self.drawcolor = (0, 0, 0)
     
-    def mainloop(self,imgName):
+    def mainloop(self):
     
         def inpt():
             word=""
@@ -44,22 +45,24 @@ class App:
                         self.drawcolor = (inpt())
                         
                     if event.key == pg.K_s:
-                        pg.image.save(self.screen, imgName)
+                        pg.image.save(self.screen, "image.bmp")
                         
                     if event.key == pg.K_l:
-                        image = pg.image.load(os.path.join(imgName))
+                        image = pg.image.load(os.path.join("image.bmp"))
                         self.screen.blit(image,(0,0))
                         
                     if event.key == pg.K_ESCAPE:
                         pg.quit()
                     
                 elif event.type == pg.MOUSEBUTTONDOWN:
-                    if event.button == 3:  
+                    if event.button == 3:  # Color picker (middle mouse button).
                         self.drawcolor = self.screen.get_at(pos)
-                       
+                        # Pick a random color.
+                        # self.drawcolor = [random.randrange(256) for _ in range(3)]
                 elif event.type == pg.MOUSEMOTION:
                     pos, rel = event.pos, event.rel
-                    if event.buttons[0]:  
+                    if event.buttons[0]:  # If the left mouse button is down.
+                        # Draw a line from the pos to the previous pos.
                         pg.draw.line(self.screen, self.drawcolor, pos, (pos[0]-rel[0], pos[1]-rel[1]), 5)
                     
 
@@ -68,6 +71,6 @@ class App:
 
 
 if __name__ == '__main__':
-    app = App(640,480)
-    app.mainloop("image.bmp")
+    app = App()
+    app.mainloop()
     pg.quit()
